88abac9008475aafaaa0c0156600ef27345497b5
Juan David Barrero <=>
2021-06-06 16:33:16 -0500

    Inicio del proyecto

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.middlewares();
+    }
+
+    middlewares() {
+
+        this.app.use(cors());
+
+        this.app.use(express.json());
+
+        this.app.use(express.static('public'));
+
+    }
+
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Escuchando en el puerto ${this.port}`);
+        });
+    }
+
+}
+
+
+module.exports = Server;
