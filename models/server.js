93cb22fef0b89fc876701be5bbe4cd516437147e
Juan David Barrero <=>
2021-06-06 17:44:57 -0500

    socket.io agregado y funcionando

--- models/server.js
+++ models/server.js
@@ -6,8 +6,12 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
 
         this.middlewares();
+
+        this.sockets();
     }
 
     middlewares() {
@@ -20,9 +24,27 @@ class Server {
 
     }
 
+    sockets() {
+
+        this.io.on('connection', socket => {
+
+            console.log("Cliente conectado", socket.id);
+
+            socket.on('disconnect', () => {
+                console.log('Cliente desconectado',socket.id);
+            });
+
+            socket.on('enviar-mensaje', (payload) => {
+                console.log("Mensaje recivido", payload);
+            });
+
+        });
+
+    }
+
 
     listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log(`Escuchando en el puerto ${this.port}`);
         });
     }
---
+++ public/js/sockets-client.js
@@ -0,0 +1,35 @@
+const lblOnline = document.getElementById('lblOnline');
+const lblOffline = document.getElementById('lblOffline');
+const txtMenssage = document.getElementById('txtMenssage');
+const btnEnviar = document.getElementById('btnEnviar');
+
+
+const socket = io();
+
+socket.on('connect', () => {
+    console.log("Conectado");
+
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+    console.log("Desconectado");
+
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+});
+
+btnEnviar.addEventListener('click', () => {
+
+    const message = txtMenssage.value; 
+
+    const payload = {
+        message,
+        id: '4f56ad4f54da6f4ads',
+        date: new Date().getTime()
+    }
+    
+    socket.emit('enviar-mensaje',payload);
+
+});
